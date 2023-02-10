2c679ef978e97d6911dacf557a3fcff5981f146c
REJOINT <brecab120@gmail.com>
2023-02-09 20:52:28 -0500

    Rendered Index Page

--- controllers/bread.js
+++ controllers/bread.js
@@ -3,8 +3,11 @@ const router = require('express').Router()
 const Bread = require('../models/bread')
 
 router.get('/', (req, res) => {
-    res.send(Bread)
-})
+    res.render('index', {
+    breads: Bread }
+    
+    )
+    })
 
 router.get('/:arrayIndex', (req, res) => {
     const { arrayIndex } = req.params
--- index.js
+++ index.js
@@ -6,6 +6,12 @@ const breadRoutes = require('./controllers/bread')
 
 const app = express()
 
+// MIDDLEWARE
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
+
 app.use('/breads', breadRoutes)
 
 const PORT= process.env.PORT
